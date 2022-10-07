8f1a4be8b5d69fb696eebdb90b4b48d9c4aa3b3e
TecTrixer <tonihoevedes@gmail.com>
2022-10-07 23:36:05 +0200

    shortened import statements in io module

--- src/io.rs
+++ src/io.rs
@@ -1,4 +1,4 @@
 // Add easy_io package for IO utilities
 pub use easy_io;
-pub use easy_io::input_reader::InputReader as InRead;
-pub use easy_io::output_writer::OutputWriter as OutWrite;
+pub use easy_io::InputReader as InRead;
+pub use easy_io::OutputWriter as OutWrite;
