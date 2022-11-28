383fb809bed91bed510f168afbc3a2ce60ea53e2
TecTrixer <tonihoevedes@gmail.com>
2022-11-28 23:39:28 +0100

    added bigint library rug to utils (+ floats, ...)

--- src/utils.rs
+++ src/utils.rs
@@ -10,13 +10,16 @@
 /// let n: usize = 10;
 /// assert!(radix(n, 2).to_string() == "1010");
 /// ```
-pub use radix_fmt::{radix, Radix};
+pub use radix_fmt::radix;
+
+#[doc(inline)]
+pub use radix_fmt::Radix;
 
 #[doc(inline)]
 /// Library for computing the md5 hash
 ///
 /// # Example
-/// 
+///
 /// ```
 /// use crate::cp_rs::utils::*;
 ///
@@ -41,3 +44,60 @@ pub use md5;
 /// assert!(fib(5) == 8)
 /// ```
 pub use cached::proc_macro::cached;
+
+#[doc(inline)]
+/// Struct for handling complex numbers
+///
+/// # Example
+///
+/// ```
+/// use crate::cp_rs::utils::*;
+///
+/// // c = 1 + 2i (with precision 53)
+/// let c = Complex::with_val(53, (1, 2));
+/// assert!(*c.real() == 1.0);
+/// assert!(*c.imag() == 2.0);
+/// ```
+pub use rug::Complex;
+
+#[doc(inline)]
+/// Struct for handling big integers
+///
+/// # Example
+///
+/// ```
+/// use crate::cp_rs::utils::*;
+///
+/// let mut num = Integer::from(u128::MAX);
+/// num *= 10;
+/// assert!(num % 10 == 0);
+/// ```
+pub use rug::Integer;
+
+#[doc(inline)]
+/// Struct for handling arbitrary precision floats
+///
+/// # Example
+///
+/// ```
+/// use crate::cp_rs::utils::*;
+///
+/// let float = Float::with_val(53, 4);
+/// let sqrt  = float.sqrt();
+/// assert!(sqrt == 2.0);
+/// ```
+pub use rug::Float;
+
+#[doc(inline)]
+/// Struct for handling arbitrary precision floats
+///
+/// # Example
+///
+/// ```
+/// use crate::cp_rs::utils::*;
+///
+/// let mut rational = Rational::from((-2, 3));
+/// rational *= 3;
+/// assert!(rational == -2);
+/// ```
+pub use rug::Rational;
